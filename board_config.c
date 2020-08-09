7ad2a83a7e1469169f6a5658c8b20d13439ba363
Leewoniu <37465374+Leewoniu@users.noreply.github.com>
2020-08-09 14:35:10 +0800

    Create board_config.c
    第一次使用git创建文件，配置文件

---
+++ board_config.c
@@ -0,0 +1,21 @@
+#include 
+
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+/*
+ *  板级配置选择
+ */
+#define CONFIG_BOARD_AC630X_DEMO //用来CONFIG_BOARD_KEYBOARD
+//#define CONFIG_BOARD_AC6302A_MOUSE //用来config mouse
+ //#define CONFIG_BOARD_AC6319A_MOUSE
+// #define CONFIG_BOARD_AC6313_DEMO
+//#define CONFIG_BOARD_AC6318_DEMO  //config 自拍器
+
+#include "board_ac630x_demo_cfg.h"
+#include "board_ac6302a_mouse_cfg.h"
+#include "board_ac6319a_mouse_cfg.h"
+#include "board_ac6313_demo_cfg.h"
+#include "board_ac6318_demo_cfg.h"
+
+#endif
